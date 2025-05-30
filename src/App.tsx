d6c7ae5bfa06604693baff2f257802aca91dcdb0
katsiev1997 <katsiev1997@gmail.com>
2025-05-30 16:49:15 +0300

    fetch posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+const fetchPosts = async () => {
+	try {
+		const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+		const data = await response.json();
+		return data.slice(0, 5); // Load only first 5 posts
+	} catch (error) {
+		console.error("Error:", error);
+	}
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [posts, setPosts] = useState<{ title: string; body: string }[]>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="text-start flex flex-col gap-4 items-start">
+			<h1 className="text-3xl font-bold">React App with Tailwind CSS</h1>
+			<button
+				className="border rounded bg-fuchsia-500 hover:bg-fuchsia-500/50 px-4 py-2 cursor-pointer"
+				onClick={() => fetchPosts().then(setPosts)}
+			>
+				Fetch Posts
+			</button>
+			<ul className="flex flex-col items-start gap-4">
+				{posts.map((post, index) => (
+					<li className="flex flex-col gap-2 items-start" key={index}>
+						<h2 className="first-letter:uppercase text-lg font-bold">{post.title}</h2>
+						<p className="font-light">{post.body}</p>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
-export default App
+export default App;
